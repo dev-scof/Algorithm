95e4c76a93216b0b4735bed1583051073cf69e2c
Scof <dev.scof.0237@gmail.com>
2023-04-15 02:26:56 +0900

    Create 시소 짝꿍.py

---
+++ programmers_algorithm/python/LEVEL_2/시소 짝꿍.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+def solution(weights):
+    answer = 0
+    info = defaultdict(int)
+    for w in weights:
+        answer += info[w]
+        answer += info[w*2]
+        answer += info[w/2]
+        answer += info[(w*2)/3]
+        answer += info[(w*3)/2]
+        answer += info[(w*4)/3]
+        answer += info[(w*3)/4]
+        info[w] += 1
+    return answer
