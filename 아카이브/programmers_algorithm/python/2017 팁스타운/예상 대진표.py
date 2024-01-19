3b067253b81fdfadacf8a0c12a7ce0cb11535a15
Scof <dev.scof.0237@gmail.com>
2024-01-20 04:07:48 +0900

    Update 예상 대진표.py

--- 아카이브/programmers_algorithm/python/2017 팁스타운/예상 대진표.py
+++ 아카이브/programmers_algorithm/python/2017 팁스타운/예상 대진표.py
@@ -1,9 +1,8 @@
-from math import ceil
 def solution(n,a,b):
     answer=0
-    while ceil(a) != ceil(b):
-        a/=2
-        b/=2
+    a, b = a-1, b-1
+    while a != b:
+        a//=2
+        b//=2
         answer+=1
-    return answer    
-print(solution(8,4,7))
+    return answer
