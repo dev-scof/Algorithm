bd44aa3e9b35d53ac6190b2ce55e19fa067575c8
Scof <dev.scof.0237@gmail.com>
2024-02-07 03:10:20 +0900

    Update n^2 배열 자르기.py

--- programmers/월간 코드 챌린지 시즌3/n^2 배열 자르기.py
+++ programmers/월간 코드 챌린지 시즌3/n^2 배열 자르기.py
@@ -1,24 +1,18 @@
 def solution(n, left, right):
     answer = []
-    arr = range(1, n+1)
-    rpt, cnt = 1, 0
-    for i in range(n):
-        rpt_cnt=0
-        for j in range(n):
-            if left <= cnt <= right:
-                if rpt_cnt < rpt:
-                    answer.append(arr[i])
-                else:
-                    answer.append(arr[j])
-            rpt_cnt += 1
-            cnt += 1
-        rpt += 1
-
+    while left <= right:
+        l, c, = divmod(left, n)
+        if c < l+1:
+            answer.append(l+1)
+        else:
+            answer.append(c+1)
+        left += 1
     return answer
 
 if __name__ == '__main__':
-    print(solution(3, 2, 5))
-    print(solution(4, 7, 14))
+    # print(solution(3, 2, 5))
+    # print(solution(4, 7, 14))
+    print(solution(5, 7, 23))
 
 '''
 [
