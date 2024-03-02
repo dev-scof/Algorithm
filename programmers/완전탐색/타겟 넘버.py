7da730c2f2b5dedc01e7d54b2e58368e9c95efbd
Scof <dev.scof.0237@gmail.com>
2024-03-02 19:41:27 +0900

    Update 타겟 넘버.py

--- programmers/완전탐색/타겟 넘버.py
+++ programmers/완전탐색/타겟 넘버.py
@@ -1,6 +1,18 @@
 answer = 0
 
 
+def dfs1(numbers, cur_array, target):
+    global answer
+    if not numbers:
+        print(cur_array)
+        if sum(cur_array) == target:
+            answer += 1
+        return
+    number = numbers.pop(0)
+    dfs1(numbers, cur_array + [number], target)
+    dfs1(numbers, cur_array + [-number], target)
+    numbers.append(number)
+
 def dfs(numbers, cur_array, target):
     global answer
     if not numbers:
@@ -11,11 +23,25 @@ def dfs(numbers, cur_array, target):
     dfs(numbers[1:], cur_array + [-numbers[0]], target)
 
 
+def dfs2(numbers, cur_array, target):
+    # 이 코드도 가능함.
+    global answer
+    if not numbers:
+        print(cur_array)
+        if sum(cur_array) == target:
+            answer += 1
+        return
+    number = numbers.pop(0)
+    dfs2(numbers, cur_array + [number], target)
+    dfs2(numbers, cur_array + [-number], target)
+    numbers.insert(0, number)
+
+
 def solution(numbers, target):
-    dfs(numbers, [], target)
+    dfs2(numbers, [], target)
     return answer
 
 
 if __name__ == '__main__':
     # print(solution([1, 1, 1, 1, 1], 3))
-    print(solution([4, 1, 2, 1], 4))
+    print(solution([4, 1, 2, 3], 4))
